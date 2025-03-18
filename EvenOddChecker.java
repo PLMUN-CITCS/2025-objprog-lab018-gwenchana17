bb4a02c98e5d8e88a18e8372939d075c405c4774
gwenchana17 <dicayhanalyn_bscs@plmun.edu.ph>
2025-03-18 21:49:06 +0800

    Submitting OBJPROG Week 05 - Laboratory # 18

---
+++ EvenOddChecker.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+
+public class EvenOddChecker {
+
+
+    // Method to get an integer input from the user
+    public static int getIntegerInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+        return number;
+    }
+
+
+    // Method to check if the given number is even or odd
+    public static String checkEvenOrOdd(int number) {
+
+
+        if (number % 2 == 0) {
+            return number + " is an Even number.";
+        } else {
+            return number + " is an Odd number.";
+        }
+    }
+
+
+    // Main method to run the program
+    public static void main(String[] args) {
+        int number = getIntegerInput();
+        String result = checkEvenOrOdd(number);
+        System.out.println(result);
+    }
+}
